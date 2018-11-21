a92265b8e0c57361bfd439d1265fb5fd5ca47536
Bojorge <duendenener@gmail.com>
2018-11-21 02:55:03 -0600

    Mapa

---
+++ app/src/main/java/com/example/manu/trazarrutas/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.manu.trazarrutas;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements MapasFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+////////////////////////////////////////////////////////////////////
+        Fragment fragmento=new MapasFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor,fragmento).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
